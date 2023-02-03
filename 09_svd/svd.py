ddfa303912ea2234e5393a013740eb3079c36bf7
vpbmac <vitorpbarbosa7@gmail.com>
2023-02-03 17:19:53 -0300

    fixed transpositions for svd

--- 09_svd/svd.py
+++ 09_svd/svd.py
@@ -19,6 +19,18 @@ sig2 = np.sqrt(eigvals[1])
 
 null = null_space(Al.T)/norm(null_space(Al.T))
 null_ = null[:,0]
-U = ar([(1/sig1)*Al@v1, (1/sig2)*Al@v2, null_])
 
-print(U)
+# >> Decomposition Components
+U = ar([(1/sig1)*Al@v1, (1/sig2)*Al@v2, null_]).T
+print(f'\n U is {U}')
+
+V = ar([v1,v2])
+print(f'\n V is {V}')
+
+Sigma = ar([[sig1,0],[0,sig2],[0,0]])
+print('Sigma is {Sigma}')
+
+# >> Multiplication
+X = U @ Sigma @ V.T
+
+print(X)
